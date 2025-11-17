e5a51cd1186d96e1ad293a6df6fe54982d4b6503
cristiannzx <cruzmonturo@gmail.com>
2025-11-16 19:26:55 -0500

    Add files via upload

---
+++ sopa.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="letras/letras.css">
+    <title>Sopa de Letras Mejorada</title>
+</head>
+<body>
+
+<h1>Sopa de Letras</h1>
+
+<?php
+
+$a = 3;
+for($i=0; $i<$a; $i++) {
+    echo $i."<br>";
+}
+?>
+
+
+</body>
+</html>
